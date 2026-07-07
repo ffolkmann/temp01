b0e7421cb7e17206eb41bf6461ed55c5078a8418
root <root@codexpress.cloud>
2026-07-07 18:26:11 +0000

    tests: unas-warehouse teszt a parity path-uritese ele (ABC-sorrend); conftest vissza (m26)

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-"""Elore betoltjuk a valos app-modulokat, mielott a test_sync_parity
-modul-szintu app.__path__-uritese (izolacios trukk) elerhetetlenne tenne oket
-a kesobb kollektalt tesztek szamara. Az import a sys.modules-cache-be kerul,
-onnan a kesobbi importok mar path-fuggetlenul kiszolgalhatok. (m26)"""
-import app.services.live_product  # noqa: F401
